c735ac0cb855afdd9ad89bc3ee267009bd7eaa84
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2025-10-23 13:57:25 +0200

    add getters and setters for Document attributes

--- src/bibliotheque/Document.java
+++ src/bibliotheque/Document.java
@@ -12,4 +12,28 @@ public abstract class Document {
     }
 
     public abstract void afficherInfos();
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getAnneeDePublication() {
+        return anneeDePublication;
+    }
+
+    public void setAnneeDePublication(String anneeDePublication) {
+        this.anneeDePublication = anneeDePublication;
+    }
 }
